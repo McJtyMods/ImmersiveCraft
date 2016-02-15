8c3b3a19d32f7a66b5dfe973716ba207c9008b30
McJty <mcjty1@gmail.com>
2016-02-15 09:40:59 +0100

    Version 1.1.0

--- src/main/java/mcjty/immcraft/ImmersiveCraft.java
+++ src/main/java/mcjty/immcraft/ImmersiveCraft.java
@@ -33,7 +33,7 @@ public class ImmersiveCraft {
 
     public static final String MODID = "immcraft";
     public static final String MODNAME = "ImmersiveCraft";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.1.0";
 
     @SidedProxy
     public static CommonProxy proxy;
