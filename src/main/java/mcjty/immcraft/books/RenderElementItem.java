dbbee963a31d40e6add25c1cb210bc246d47457e
McJty <mcjty1@gmail.com>
2017-01-25 19:42:52 +0100

    Items align well with the text now

--- src/main/java/mcjty/immcraft/books/RenderElementItem.java
+++ src/main/java/mcjty/immcraft/books/RenderElementItem.java
@@ -2,11 +2,16 @@ package mcjty.immcraft.books;
 
 import mcjty.lib.tools.ItemStackTools;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.*;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
-import org.lwjgl.opengl.GL11;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class RenderElementItem implements RenderElement {
 
@@ -14,10 +19,17 @@ public class RenderElementItem implements RenderElement {
     private final int x;
     private final int y;
 
+    private final ItemModelMesher itemModelMesher;
+    private final TextureManager textureManager;
+
+
     public RenderElementItem(ItemStack item, int x, int y) {
         this.item = item;
         this.x = x;
         this.y = y;
+
+        itemModelMesher = Minecraft.getMinecraft().getRenderItem().getItemModelMesher();
+        textureManager = Minecraft.getMinecraft().getTextureManager();
     }
 
     @Override
@@ -26,7 +38,6 @@ public class RenderElementItem implements RenderElement {
             return;
         }
         RenderHelper.enableGUIStandardItemLighting();
-//        RenderHelper.enableStandardItemLighting();
         GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 
         GlStateManager.depthMask(true);
@@ -35,14 +46,9 @@ public class RenderElementItem implements RenderElement {
         GlStateManager.enableDepth();
 
         GlStateManager.pushMatrix();
-        GlStateManager.translate(-80, -1000, -750);
-        GlStateManager.scale(5, 5, 5);
+        GlStateManager.translate(0, 0, -150);
 
-//        short short1 = 240;
-//        short short2 = 240;
-//        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, short1 / 1.0F, short2 / 1.0F);
-        renderSlot((y+dy), item, x/3);
-        renderSlotOverlay(Minecraft.getMinecraft().fontRendererObj, (y+dy), item, x/3);
+        renderSlot(item, x, y + dy);
 
         GlStateManager.popMatrix();
 
@@ -50,71 +56,56 @@ public class RenderElementItem implements RenderElement {
         RenderHelper.enableStandardItemLighting();
     }
 
-    private void renderSlot(int currenty, ItemStack itm, int x) {
-        RenderItem itemRender = Minecraft.getMinecraft().getRenderItem();
-        itemRender.renderItemAndEffectIntoGUI(itm, x, currenty);
-    }
-
-    private void renderSlotOverlay(FontRenderer fontRenderer, int currenty, ItemStack itm, int x) {
-//                itemRender.renderItemOverlayIntoGUI(fontRenderer, Minecraft.getMinecraft().getTextureManager(), itm, x, currenty);
-        renderItemOverlayIntoGUI(fontRenderer, itm, x, currenty);
-    }
-
-    private static void renderItemOverlayIntoGUI(FontRenderer fontRenderer, ItemStack itemStack, int x, int y) {
-        int size = ItemStackTools.getStackSize(itemStack);
-        if (size > 1) {
-            String s1;
-            if (size < 10000) {
-                s1 = String.valueOf(size);
-            } else if (size < 1000000) {
-                s1 = String.valueOf(size / 1000) + "k";
-            } else if (size < 1000000000) {
-                s1 = String.valueOf(size / 1000000) + "m";
-            } else {
-                s1 = String.valueOf(size / 1000000000) + "g";
-            }
-            GlStateManager.disableLighting();
-//                GL11.glDisable(GL11.GL_DEPTH_TEST);
-            GlStateManager.disableBlend();
-            fontRenderer.drawString(s1, x + 19 - 2 - fontRenderer.getStringWidth(s1), y + 6 + 3, 16777215);
-            GlStateManager.enableLighting();
-//                GL11.glEnable(GL11.GL_DEPTH_TEST);
-        }
-
-        if (itemStack.getItem().showDurabilityBar(itemStack)) {
-            double health = itemStack.getItem().getDurabilityForDisplay(itemStack);
-            int j1 = (int) Math.round(13.0D - health * 13.0D);
-            int k = (int) Math.round(255.0D - health * 255.0D);
-            GlStateManager.disableLighting();
-//                GL11.glDisable(GL11.GL_DEPTH_TEST);
-            GlStateManager.disableTexture2D();
-            GlStateManager.disableAlpha();
-            GlStateManager.disableBlend();
-            Tessellator tessellator = Tessellator.getInstance();
-            int l = 255 - k << 16 | k << 8;
-            int i1 = (255 - k) / 4 << 16 | 16128;
-            renderQuad(tessellator, x + 2, y + 13, 13, 2, 0, 0.0D);
-            renderQuad(tessellator, x + 2, y + 13, 12, 1, i1, 0.02D);
-            renderQuad(tessellator, x + 2, y + 13, j1, 1, l, 0.04D);
-            //GL11.glEnable(GL11.GL_BLEND); // Forge: Disable Bled because it screws with a lot of things down the line.
-            GlStateManager.enableAlpha();
-            GlStateManager.enableTexture2D();
-            GlStateManager.enableLighting();
-//                GL11.glEnable(GL11.GL_DEPTH_TEST);
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+    private void renderSlot(ItemStack itm, int x, int currenty) {
+        try {
+            renderItemModelIntoGUI(itm, x, currenty, getItemModelWithOverrides(itm, null, Minecraft.getMinecraft().player));
+        } catch (Throwable throwable) {
         }
     }
 
-    private static void renderQuad(Tessellator tessellator, int x, int y, int width, int height, int color, double offset) {
-        VertexBuffer buffer = tessellator.getBuffer();
-        buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION);
-//        tessellator.setColorOpaque_I(color);
-        buffer.pos(x, y, offset);
-        buffer.pos(x, (y + height), offset);
-        buffer.pos((x + width), (y + height), offset);
-        buffer.pos((x + width), y, offset);
-        tessellator.draw();
+
+    private void renderItemModelIntoGUI(ItemStack stack, int x, int y, IBakedModel bakedmodel) {
+        GlStateManager.pushMatrix();
+        this.textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+        this.textureManager.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).setBlurMipmap(false, false);
+        GlStateManager.enableRescaleNormal();
+        GlStateManager.enableAlpha();
+        GlStateManager.alphaFunc(516, 0.1F);
+        GlStateManager.enableBlend();
+        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+        this.setupGuiTransform(x, y, bakedmodel.isGui3d());
+        bakedmodel = net.minecraftforge.client.ForgeHooksClient.handleCameraTransforms(bakedmodel, ItemCameraTransforms.TransformType.GUI, false);
+        Minecraft.getMinecraft().getRenderItem().renderItem(stack, bakedmodel);
+        GlStateManager.disableAlpha();
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.disableLighting();
+        GlStateManager.popMatrix();
+        this.textureManager.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+        this.textureManager.getTexture(TextureMap.LOCATION_BLOCKS_TEXTURE).restoreLastBlurMipmap();
     }
+
+
+    private IBakedModel getItemModelWithOverrides(ItemStack stack, @Nullable World worldIn, @Nullable EntityLivingBase entitylivingbaseIn) {
+        IBakedModel ibakedmodel = this.itemModelMesher.getItemModel(stack);
+        return ibakedmodel.getOverrides().handleItemState(ibakedmodel, stack, worldIn, entitylivingbaseIn);
+    }
+
+    private void setupGuiTransform(int xPosition, int yPosition, boolean isGui3d) {
+        GlStateManager.translate((float) xPosition, (float) yPosition, 100.0F + 50); //@@@this.zLevel);
+//        GlStateManager.translate(8.0F, 8.0F, 0.0F);
+        GlStateManager.translate(36.0F, -32.0F, 0.0F);
+        GlStateManager.scale(1.0F, -1.0F, 1.0F);
+//        GlStateManager.scale(16.0F, 16.0F, 16.0F);
+        GlStateManager.scale(64.0F, 64.0F, 64.0F);
+
+        if (isGui3d) {
+            GlStateManager.enableLighting();
+        } else {
+            GlStateManager.disableLighting();
+        }
+    }
+
 
 
 }
