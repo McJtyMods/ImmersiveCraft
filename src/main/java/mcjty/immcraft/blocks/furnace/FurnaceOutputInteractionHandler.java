a4d4db46148de6453472bec3aea0ee5753814ffa
Lanse <simon_stalnabb@hotmail.com>
2018-01-22 17:38:31 +0100

    Push

--- src/main/java/mcjty/immcraft/blocks/furnace/FurnaceOutputInteractionHandler.java
+++ src/main/java/mcjty/immcraft/blocks/furnace/FurnaceOutputInteractionHandler.java
@@ -3,6 +3,7 @@ package mcjty.immcraft.blocks.furnace;
 import mcjty.immcraft.api.handles.DefaultInterfaceHandle;
 import net.minecraft.entity.item.EntityXPOrb;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.tileentity.TileEntity;
@@ -10,16 +11,22 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class FurnaceOutputInteractionHandler extends DefaultInterfaceHandle {
-    private int slot;
-    private int removeCount;
 
     public FurnaceOutputInteractionHandler(String selectorID) {
         super(selectorID);
     }
 
+    @Override
+    public ItemStack extractOutput(TileEntity genericTE, EntityPlayer player, int amount) {
+        ItemStack stack = super.extractOutput(genericTE, player, amount);
+        spawnExperience(player, stack);
+        FMLCommonHandler.instance().firePlayerSmeltedEvent(player, stack);
+        return stack;
+    }
+
     public void spawnExperience(EntityPlayer player, ItemStack itemStack) {
         if (player.world.isRemote) {
-            int i = this.removeCount;
+            int i = itemStack.getCount();
             float f = FurnaceRecipes.instance().getSmeltingExperience(itemStack);
 
             if (f == 0.0F) {
@@ -40,13 +47,5 @@ public class FurnaceOutputInteractionHandler extends DefaultInterfaceHandle {
                 player.world.spawnEntity(new EntityXPOrb(player.world, player.posX, player.posY + 0.5D, player.posZ + 0.5D, k));
             }
         }
-    }
-
-    @Override
-    public ItemStack extractOutput(TileEntity genericTE, EntityPlayer player, int amount) {
-        ItemStack itemStack = super.extractOutput(genericTE, player, amount);
-        spawnExperience(player, itemStack);
-        FMLCommonHandler.instance().firePlayerSmeltedEvent(player, itemStack);
-        return itemStack;
     }
 }
