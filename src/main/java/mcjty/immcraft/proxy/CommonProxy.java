60ca24ae8ca356efdfa3df49d86405410031d558
McJty <mcjty1@gmail.com>
2018-05-14 11:09:04 +0200

    Rationalized the proxies. There is now an AbstractCommonProxy in McJtyLib

--- src/main/java/mcjty/immcraft/proxy/CommonProxy.java
+++ src/main/java/mcjty/immcraft/proxy/CommonProxy.java
@@ -1,6 +1,5 @@
 package mcjty.immcraft.proxy;
 
-import com.google.common.util.concurrent.ListenableFuture;
 import mcjty.immcraft.ImmersiveCraft;
 import mcjty.immcraft.blocks.ModBlocks;
 import mcjty.immcraft.config.ConfigSetup;
@@ -8,10 +7,8 @@ import mcjty.immcraft.events.ForgeEventHandlers;
 import mcjty.immcraft.items.ModItems;
 import mcjty.immcraft.network.ImmCraftPacketHandler;
 import mcjty.immcraft.worldgen.WorldGen;
-import mcjty.lib.McJtyLib;
 import mcjty.lib.network.PacketHandler;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.world.World;
+import mcjty.lib.proxy.AbstractCommonProxy;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -19,13 +16,13 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 
-import java.util.concurrent.Callable;
+public class CommonProxy extends AbstractCommonProxy {
 
-public class CommonProxy {
-
+    @Override
     public void preInit(FMLPreInitializationEvent e) {
+        super.preInit(e);
+
         MinecraftForge.EVENT_BUS.register(new ForgeEventHandlers());
-        McJtyLib.preInit(e);
 
         SimpleNetworkWrapper network = PacketHandler.registerMessages(ImmersiveCraft.MODID, "immcraft");
         ImmCraftPacketHandler.registerMessages(network);
@@ -36,29 +33,15 @@ public class CommonProxy {
         WorldGen.init();
     }
 
+    @Override
     public void init(FMLInitializationEvent e) {
+        super.init(e);
         NetworkRegistry.INSTANCE.registerGuiHandler(ImmersiveCraft.instance, new GuiProxy());
     }
 
+    @Override
     public void postInit(FMLPostInitializationEvent e) {
+        super.postInit(e);
         ConfigSetup.postInit();
     }
-
-    public World getClientWorld() {
-        throw new IllegalStateException("This should only be called from client side");
-    }
-
-    public EntityPlayer getClientPlayer() {
-        throw new IllegalStateException("This should only be called from client side");
-    }
-
-    public <V> ListenableFuture<V> addScheduledTaskClient(Callable<V> callableToSchedule) {
-        throw new IllegalStateException("This should only be called from client side");
-    }
-
-    public ListenableFuture<Object> addScheduledTaskClient(Runnable runnableToSchedule) {
-        throw new IllegalStateException("This should only be called from client side");
-    }
-
-
 }
