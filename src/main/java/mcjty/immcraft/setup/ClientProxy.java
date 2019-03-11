fa778320b8acdd7e1c804cca795c0973340b7389
McJty <mcjty1@gmail.com>
2019-03-11 11:41:13 +0100

    More cleanups and removal of unneeded things

--- src/main/java/mcjty/immcraft/setup/ClientProxy.java
+++ src/main/java/mcjty/immcraft/setup/ClientProxy.java
@@ -6,7 +6,6 @@ import mcjty.immcraft.blocks.bundle.BundleModelLoader;
 import mcjty.immcraft.events.ClientForgeEventHandlers;
 import mcjty.immcraft.input.InputHandler;
 import mcjty.immcraft.input.KeyBindings;
-import mcjty.lib.McJtyLibClient;
 import mcjty.lib.font.FontLoader;
 import mcjty.lib.font.TrueTypeFont;
 import mcjty.lib.setup.DefaultClientProxy;
@@ -16,7 +15,6 @@ import net.minecraftforge.client.model.obj.OBJLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import java.awt.Font;
@@ -30,7 +28,6 @@ public class ClientProxy extends DefaultClientProxy {
     @Override
     public void preInit(FMLPreInitializationEvent e) {
         super.preInit(e);
-        McJtyLibClient.preInit(e);
 
         MinecraftForge.EVENT_BUS.register(new ClientForgeEventHandlers());
         OBJLoader.INSTANCE.addDomain(ImmersiveCraft.MODID);
@@ -50,10 +47,5 @@ public class ClientProxy extends DefaultClientProxy {
         font_italic = FontLoader.createFont(new ResourceLocation(ImmersiveCraft.MODID, "fonts/ubuntu_italic.ttf"), 64, false,
                 Font.TRUETYPE_FONT, new char[] { '\u2022', '\u2014' });
         ModBlocks.initItemModels();
-    }
-
-    @Override
-    public void postInit(FMLPostInitializationEvent e) {
-        super.postInit(e);
     }
 }
